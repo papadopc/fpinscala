4dd62f77635461fbf90e80f931d31da753e9f4a7
Chris Papadopoulos <papadopc@gmail.com>
2016-04-02 17:25:47 -0700

    Finished Chapter 4: Option

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -24,7 +24,7 @@ sealed trait Option[+A] {
   }
 
   def filter(f: A => Boolean): Option[A] = this match {
-    case Some(a) => if f(a) => this
+    case Some(a) if f(a) => this
     case _ => None
   }
 }
@@ -52,11 +52,23 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) flatMap(m=>mean(xs.map(x=>math.pow(x-m,2))))
+  }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = (a,b) match {
+    case (_,None) => None
+    case (None,_) => None
+    case (Some(a),Some(b)) => Some(f(a,b))
+  }
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h::t  => h flatMap ( hh => sequence(t) map (hh:: _))
+  }
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h::t => map2(f(h),traverse(t)(f))( _ :: _)
+  }
 }
